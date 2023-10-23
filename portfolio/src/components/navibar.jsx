369e366a7ab706e74a30f00d541857468c0e2b35
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-10-23 19:10:56 -0400

    adding nav bar buttons

--- portfolio/src/components/navibar.jsx
+++ portfolio/src/components/navibar.jsx
@@ -1,15 +1,19 @@
 import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Nav } from 'react-bootstrap/Nav';
+
 export default function NaviBar(){
 
   return(
     <Navbar bg="light" expand="lg" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="/">Moses Njoroge</Navbar.Brand>
-          {/* <Nav className="me-auto justify-content-end">
+          <Nav className="me-auto justify-content-end">
             <Nav.Link href="#features">Case Studies</Nav.Link>
-          </Nav> */}
+            <Nav.Link href="#features">Projects</Nav.Link>
+            <Nav.Link href="#features">About</Nav.Link>
+            <Nav.Link href="#features">Contact</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
   );
