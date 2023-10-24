65c46cf3b6d2a95f16118c4fe2539d87a4cc7722
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-10-24 09:12:41 -0400

    project card redesign

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -9,6 +9,7 @@ import {
 } from "react-scroll-motion";
 
 
+
 function App() {
 
   return (
@@ -23,16 +24,28 @@ function App() {
           <ScrollPage ScrollPage={1}>
             <div className='pt-3'>
               <h2 className="text-center">
-                Case Studies
+                Projects
               </h2>
             </div>
-            <div className="card-trip">
-              <img src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694511079/portfolio/Screenshot_2023-09-12_at_5.29.31_AM_amvykb.png" />
-              <div className="card-trip-infos">
-                <div type='button' >
-                  Sidequests
+            <div>
+              <div className='container pt-4'>
+                <div className="card-product">
+                  <img src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694537478/portfolio/Screenshot_2023-09-12_at_12.47.14_PM_sx6w2u.png"/>
+                  <div className="card-product-infos">
+                    <h2>SideQuests</h2>
+                    <p>
+                      A mobile web app where users can personalize their road trip by adding stops based on their interests.
+                      One of the main challenges was figuring out a way to add stops based on user selection and having that reflected on the Mapbox api.
+                    </p>
+                  </div>
                 </div>
               </div>
+              <div className='project-card-img'>
+                {/* <img  style={{height:  "300px"}} src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694537478/portfolio/Screenshot_2023-09-12_at_12.47.14_PM_sx6w2u.png"/> */}
+                {/* <img  style={{height:  "200px"}} src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694537476/portfolio/Screenshot_2023-09-12_at_12.47.43_PM_ep5ojc.png"/> */}
+                {/* <img  style={{height:  "200px"}} src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694537477/portfolio/Screenshot_2023-09-12_at_12.49.18_PM_crikxa.png"/> */}
+              </div>
+                {/* <!-- SideQuest carousel --> */}
             </div>
           </ScrollPage>
 
