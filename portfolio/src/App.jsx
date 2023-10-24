89ca39ce0d9b0f7b7c62853bb4b6a045525094a3
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-10-24 10:29:22 -0400

    temp build format:scroll spy & 3d to be integrated

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -7,7 +7,7 @@ import {
   ScrollContainer,
   ScrollPage
 } from "react-scroll-motion";
-
+import { Form } from 'react-bootstrap';
 
 
 function App() {
@@ -28,7 +28,7 @@ function App() {
               </h2>
             </div>
             <div>
-              <div className='container pt-4'>
+              <div className='container pt-5'>
                 <div className="card-product">
                   <img src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694537478/portfolio/Screenshot_2023-09-12_at_12.47.14_PM_sx6w2u.png"/>
                   <div className="card-product-infos">
@@ -37,59 +37,88 @@ function App() {
                       A mobile web app where users can personalize their road trip by adding stops based on their interests.
                       One of the main challenges was figuring out a way to add stops based on user selection and having that reflected on the Mapbox api.
                     </p>
+                    <a target="_blank"  rel="noreferrer" className="btn btn-flat" role="button"  href="https://github.com/mosesnjoroge/SideQuest">
+                        See More
+                    </a>
                   </div>
-                </div>
-              </div>
-              <div className='project-card-img'>
-                {/* <img  style={{height:  "300px"}} src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694537478/portfolio/Screenshot_2023-09-12_at_12.47.14_PM_sx6w2u.png"/> */}
-                {/* <img  style={{height:  "200px"}} src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694537476/portfolio/Screenshot_2023-09-12_at_12.47.43_PM_ep5ojc.png"/> */}
-                {/* <img  style={{height:  "200px"}} src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694537477/portfolio/Screenshot_2023-09-12_at_12.49.18_PM_crikxa.png"/> */}
-              </div>
-                {/* <!-- SideQuest carousel --> */}
-            </div>
-          </ScrollPage>
-
-          <ScrollPage ScrollPage={2}>
-            <div className='section-2'>
-                <div className="card-trip">
-                  <img src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694511079/portfolio/Screenshot_2023-09-12_at_5.29.31_AM_amvykb.png" />
-                  <div className="card-trip-infos">
-                    <div type='button' >
-                      Sidequests
+                  {/* <div className="card-product">
+                    <img src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694537478/portfolio/Screenshot_2023-09-12_at_12.47.14_PM_sx6w2u.png"/>
+                    <div className="card-product-infos">
+                      <h2>SideQuests</h2>
+                      <p>
+                        An Airbnb clone marketplace concept, where users can rent dogs for their required needs or rent out their own dogs to other users. Built with Ruby on Rails framework. Front-end re-imagined from one of the final group projects i took part in.
+                      </p>
                     </div>
                   </div>
+                  <div className="card-product">
+                    <img src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694537478/portfolio/Screenshot_2023-09-12_at_12.47.14_PM_sx6w2u.png"/>
+                    <div className="card-product-infos">
+                      <h2>SideQuests</h2>
+                      <p>
+                        A Dynamic web application that not only allows users to search for their favourite movies but also create a curated list of movies of interest. With a variety of user preference customization options. Built with Ruby on Rails, with postgresql db, bootstrapJS, stimulusJS.
+                      </p>
+                    </div>
+                  </div> */}
                 </div>
+              </div>
             </div>
           </ScrollPage>
-
-
-          <ScrollPage ScrollPage={3}>
-              {/* <Projects/> */}
-              <div className="card-trip">
-                <img src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694503127/portfolio/Screenshot_2023-09-12_at_3.14.16_AM_juoxvy.png" />
-                <div className="card-trip-infos">
-                  <div type='button' >
-                    Whoof
-                  </div>
+          <ScrollPage ScrollPage={2}>
+            <div className='container pt-5'>
+              <div className="card-product about-card">
+                <div >
+                  <img src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694367324/portfolio/profilephoto_efbve2.jpg" alt="Profile Photo" />
+                </div>
+                <div className="card-product-infos">
+                  <h2>About Me</h2>
+                  <p>
+                    I am a Full-Stack Software Developer, with a user-centeric approach to developing software.
+                    When I am not coding I can be found at the nearest gym or art gallery.
+                    If I was to describe myself as a person who is also curious and eager to learn, creative thinker always looking for efficiencies, and likes to take an initiative.
+                    Interested to learn more about me and work process I would love to get in touch.
+                  </p>
                 </div>
               </div>
-            {/* <Animator animation={batch(FadeIn(), FadeOut())}>
-            </Animator> */}
+            </div>
           </ScrollPage>
-
-          <ScrollPage ScrollPage={4}>
-              {/* <Projects/> */}
-            <div className="card-trip">
-              <img src="https://user-images.githubusercontent.com/102884273/229380795-0fcef9dc-460f-44af-bc7b-e206538dee38.png" />
-              <div className="card-trip-infos">
-                <div type='button' >
-                  Notes
-                </div>
-              </div>
+          <ScrollPage ScrollPage={3}>
+            <div className='container pt-5'>
+              <h2 className='text-center'>
+                Tech skills
+              </h2>
             </div>
           </ScrollPage>
         </div>
-        <ScrollPage>
+        <ScrollPage ScrollPage={4}>
+          <div className='container pt-5'>
+            <div>
+              <h2>
+                Contact me
+              </h2>
+            </div>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  <p>
+                    We will never share your email with anyone else.
+                  </p>
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <button className='btn btn-flat' type="submit">
+                Submit
+              </button>
+            </Form>
+          </div>
           <footer id="footr">
             <a href="#">Contact us</a>
             <a href="#">Who we are</a>
@@ -97,7 +126,6 @@ function App() {
         </ScrollPage>
       </div>
     </ScrollContainer>
-
   )
 }
 
--- portfolio/src/components/banner.jsx
+++ portfolio/src/components/banner.jsx
@@ -8,14 +8,11 @@ function Banner(){
       <div className="justify-content-center">
         <div className="bg-dark text-white">
           <CardTitle>
-          <div className = "profile-info">
-            <img className ="avatar-bordered" src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694367324/portfolio/profilephoto_efbve2.jpg" alt="Profile Photo"/>
-          </div>
+            <div className = "profile-info">
+              <img className ="avatar-bordered" src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694367324/portfolio/profilephoto_efbve2.jpg" alt="Profile Photo"/>
+            </div>
             <h1>I specialize in<strong> thinking outside of the box</strong> to <strong>find simple solutions to complex problems </strong>!</h1>
-            {/* <img className ="banner-puzzle-piece" src="https://res.cloudinary.com/dtxxea3qi/image/upload/v1694367159/portfolio/puzzle_piece_ioyejx.jpg" alt="jigsaw_puzzle"/> */}
           </CardTitle>
-          <div>
-          </div>
         </div>
       </div>
     </div>
